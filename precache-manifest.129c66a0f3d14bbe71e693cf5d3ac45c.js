912f4fb8b2ad92e15b512800c6764ad8d162a2c9
Michael Zhang <michaelzhang@fairfaxmedia.com.au>
2020-09-30 21:16:38 +1000

    Updates

---
+++ precache-manifest.129c66a0f3d14bbe71e693cf5d3ac45c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ac5e90d16741e158d71608fed93bcf",
+    "url": "/weather-widget/index.html"
+  },
+  {
+    "revision": "ffcc7308d36779364730",
+    "url": "/weather-widget/static/css/main.c1d87a49.chunk.css"
+  },
+  {
+    "revision": "ed0dea77e54fc3f86e7f",
+    "url": "/weather-widget/static/js/2.50f47ba7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-widget/static/js/2.50f47ba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffcc7308d36779364730",
+    "url": "/weather-widget/static/js/main.4683e46d.chunk.js"
+  },
+  {
+    "revision": "6284b67397b6ecf8623c",
+    "url": "/weather-widget/static/js/runtime-main.6878f9b6.js"
+  }
+]);
